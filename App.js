79013e1639e0fcd1b5e72b6f8bc63ebaf251f36a
maru <maruthavanan.s@gmail.com>
2020-04-22 19:30:51 +0530

    Basic layout and components

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{padding: 50}}>
+      <View style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      }}>
+        <TextInput 
+          placeholder="Enter your goal"
+          style={{
+            width: '80%',
+            borderColor: 'black',
+            borderWidth: 1,
+            padding: 10,
+            borderRadius: 7,
+          }}
+        />
+        <Button title="Add" />
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
