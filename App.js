2ae4f2df879b3879c7d270f35f8551abc7f124a7
maru <maruthavanan.s@gmail.com>
2020-04-22 19:53:14 +0530

    Added styles with class

--- App.js
+++ App.js
@@ -3,21 +3,11 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{padding: 50}}>
-      <View style={{
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-      }}>
-        <TextInput 
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
           placeholder="Enter your goal"
-          style={{
-            width: '80%',
-            borderColor: 'black',
-            borderWidth: 1,
-            padding: 10,
-            borderRadius: 7,
-          }}
+          style={styles.input}
         />
         <Button title="Add" />
       </View>
@@ -29,5 +19,19 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 7,
+  },
 });
